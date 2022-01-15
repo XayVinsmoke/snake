c00d7ea3c88db561ce766811e4bd5fe4b34b0a27
XayVinsmoke <s4legendex3@web.de>
2022-01-15 15:13:35 +0100

    - Basic Canvas drawing - Gameloop - User input

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 ### Snake
 
 #### Basic Snake Game for the Browser
+
+Regeln:
+    Snake 1 -> Schlange stirbt wenn sie die Wand oder sich selbst berührt -> Game Over
+    Es befindet sich immer ein Food in der Arena
+    Wenn Schlange Food aufnimmt, erhöht sich ihre Länge um 1
+
+Großes Problem -> Herunterbrechen in kleinere Probleme
+
+Großes Problem: Implementierung von Snake im Browser
+
+1) Rendering im Browser (<canvas>...) #check
+2) 'Schlange' rendern (zu Beginn Rechteck, also nur Schlangenkopf) #check
+3) Game Loop implementieren (Schlangenkopf in eine Richtung bewegen) #check
+3.1) Schlange bewegen (4 Richtungen) #check
+4) Bewegungsrichtung über Benutzereingabe #check
+5) Food spawnen
+6) Schlange kann Food einsammeln und neues Food spawnt
+(Food spawnt nicht in der Schlange aber ansonsten zufällig)
+7) Kollision Schlangenkopf mit der Wand erkennen
+8) Schlangenkörper implementieren, Körper folgt dem Kopf, wächst mit Aufnahme von Food
+9) Kollision mit dem Schlangenkörper
--- script.js
+++ script.js
@@ -1,1 +1,86 @@
-console.log("hello world");
+// speichtert das html canvas in der variablen 'canvas'
+const canvas = document.getElementById("canvas");
+
+const renderer = canvas.getContext("2d");
+
+const colorSnake = '#000000';
+const colorFood = '#aa00aa';
+const snakeSize = 20;
+const snakePos = [0, 5]; // [x-pos, y-pos]
+const foodPos = [10, 10];
+let direction = 'RIGHT' // LEFT, UP, DOWN
+let frameCounter = 0;
+
+// wenn eine Taste gedrückt wird, rufe die Funktion keyDown auf
+// (hinter den Kulissen wird der Funktion das Event übergeben
+document.onkeydown = keyDown;
+
+// function definition
+function drawSnakeHead(x, y) {
+    // ctx.fillRect(x-pos, y-pos, breite, hoehe);
+    renderer.fillStyle = colorSnake;
+    renderer.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
+}
+
+function drawFood(x, y) {
+    renderer.fillStyle = colorFood;
+    renderer.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
+}
+
+function clearCanvas() {
+    renderer.clearRect(0, 0, 720, 480);
+}
+
+function gameLoop(timeStamp) {
+    // TODO: User Input
+    frameCounter = frameCounter + 1;
+
+    // zeichne nur alle 30 Frames etwas Neues
+    if (frameCounter == 30) {
+        frameCounter = 0;
+        clearCanvas();
+        // aktualisiere die Position des Schlangenkopfes
+        if (direction == "RIGHT") {
+            snakePos[0] = snakePos[0] + 1;
+        }
+
+        if (direction == "LEFT") {
+            snakePos[0] = snakePos[0] - 1;
+        }
+
+        if (direction == "UP") {
+            snakePos[1] = snakePos[1] - 1;
+        }
+
+        if (direction == "DOWN") {
+            snakePos[1] = snakePos[1] + 1;
+        }
+
+        drawFood(foodPos[0], foodPos[1]);
+        drawSnakeHead(snakePos[0], snakePos[1]);
+    }
+
+    // naechsten Frame
+    window.requestAnimationFrame(gameLoop);
+}
+
+function keyDown(e) {
+    if (e.code == "ArrowUp") {
+        direction = "UP";
+    }
+    if (e.code == "ArrowDown") {
+        direction = "DOWN";
+    }
+    if (e.code == "ArrowLeft") {
+        direction = "LEFT";
+    }
+    if (e.code == "ArrowRight") {
+        direction = "RIGHT";
+    }
+}
+
+// draw first Frame
+drawSnakeHead(snakePos[0], snakePos[1]);
+drawFood(foodPos[0], foodPos[1]);
+window.requestAnimationFrame(gameLoop);
+
